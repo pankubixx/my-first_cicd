7c9d3224fb829c9ed5ddd435b0c08125bf11ad7d
Jakub Łączkowski <76168774+pankubixx@users.noreply.github.com>
2026-03-20 09:20:45 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_hello():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello World!!!"}
 
 def test_add_positive_numbers():
     response = client.get("/add/2/3")
